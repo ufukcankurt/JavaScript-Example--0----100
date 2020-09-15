c4176faafa22ad4d67d40f802299e4cc0de22043
ufukcankurt <ufuk.cnk@gmail.com>
2020-09-15 18:46:05 +0300

    add Karsilastirma_İfadesi

---
+++ Temeller/15_Karşılaştırma_Operatörler_Koşullar/app.js
@@ -0,0 +1,104 @@
+// Karşılaştırma Operatörleri
+
+// ==
+// ===
+// !=
+// !==
+// >
+// <
+// >=
+// <=
+
+
+// console.log(2 == 2);
+// console.log("js" == "java");
+// console.log( 2 == "2"); // sayiyi stringe cevirir. True deger döner.
+// console.log(2 === "2"); // Degerler aynı mı ve tipler aynı mı diye bakar .. bu durumda bize FALSE döner.
+
+// console.log( 4 > 2); // True
+// console.log( 2 != 4); // Eşit değil midir ?  2 eşit değil midir 4 e ?
+
+// console.log( 2 < 4);
+// console.log(2 >= 4);
+// console.log(2 >= 4);
+
+
+
+// // MANTIKSAL BAĞLAÇLAR 
+
+// //-- NOT -- // Bir şeyin değerini tam tersine cevirir.
+
+// // AND operatörü   " && "
+
+// console.log( (2 == 2) && ("Ahmet" == "Ahmet")); // True
+
+// // OR operatörü    "  ||  "
+
+// console.log( (4 == 2) || ("Ahmet" == "Ahmet"));
+
+
+
+// const error = true ;
+
+// if(error == true){
+//     console.log("Hata Olustu");
+// }
+// else{
+//     console.log("Hata olusmadi");
+// }
+
+
+// const user = "mmc";
+
+// if( user == "mmc"){
+//     console.log("Kullanici bulundu");
+// }
+// else{
+//     console.log("Kullanıcı bulunmadı.");
+// }
+
+
+
+
+// const process = 1 ;
+
+// if(process === "1"){
+//     console.log("İşlem 1");
+// }
+// else if(process == "2"){
+//     console.log("İşlem 2");
+// }
+// else if(process == "3"){
+//     console.log("İşlem 3");
+// }
+// else if(process == "4"){
+//     console.log("İşlem 4");
+// }
+// else{
+//     console.log("Gecersiz islem.");
+// }
+
+
+const number = 100;
+
+// if( number === 100){
+//     console.log("Sayi 100'e eşit");
+// }
+// else{
+//     console.log("Sayi 100'e esit degildir.")
+// }
+
+
+
+// // TERNARY OPERATOR --> eğer tek bir işlem yapıyorsak kullanırız.
+
+// console.log(number === 100 ? "Sayi 100" : "Sayi 100 değil.");
+
+
+if( number === 100)
+    console.log("Sayi 100"); // tek bir satir yazacaksak  "{}"  kullanmamıza gerek yok
+else
+console.log("sayi 100 değil"); // tek bir satir yazacaksak  "{}"  kullanmamıza gerek yok
+
+
+
